8c13cd32f2889086ac352a042d4f68d7d8f5c9b2
Johana Petit-frere <johanapetit-frere@Johanas-MBP.lan>
2024-06-19 23:17:20 +0100

    update 2

--- variables.tf
+++ variables.tf
@@ -2,11 +2,8 @@
 
 variable "bucket_name" {
   description = "Name of the S3 bucket. Must be Unique across AWS"
-  type        = string
-  default     = "utrains_24"
 }
 
 variable "region" {
   description = "This is the cloud hosting region where your webapp will be deployed."
-  default     = "us-east-1"
 }
